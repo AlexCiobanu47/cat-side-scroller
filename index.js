64a73019f02f22c4727ad9edd0dddbf99e5acd58
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-03-29 10:24:48 +0300

    added basic player movement

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.1;
+const playerSpeedX = 1;
+const playerSpeedY = 5;
 class Player {
   constructor() {
     this.position = {
@@ -42,3 +44,34 @@ function animate() {
   player.update();
 }
 animate();
+
+addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "w":
+      player.velocity.y -= playerSpeedY;
+      break;
+    case "a":
+      player.velocity.x -= playerSpeedX;
+      break;
+    case "s":
+      player.velocity.y += playerSpeedY;
+      break;
+    case "d":
+      player.velocity.x += playerSpeedX;
+      break;
+  }
+});
+addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "w":
+      break;
+    case "a":
+      player.velocity.x = 0;
+      break;
+    case "s":
+      break;
+    case "d":
+      player.velocity.x = 0;
+      break;
+  }
+});
